8b7fee0b18f83e580226054cb4c46884d6d5e3e1
Mario <mcoahuila@mimoni.com>
2016-01-25 16:34:45 -0600

    Add new view in the example

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, abort, request
 
 app = Flask(__name__)
 
+# Index view to the main URL
 @app.route('/')
 def index():
     return ('<h3>Hello world with Flask framework!!</h3>')
+
+# View to send an specific name to be displayed on browser
+@app.route('/hello/<name>')
+@app.route('/hello/')
+def hello(name=None):
+    if name is None:
+        # If no name is specified, it tries to obtain
+        # from the URL string
+        name = request.args.get('name')
+        if name:
+            return ('Hello ' + name)
+        else:
+            return ('Any name was found')
+
+    else:
+        # No name was specified in the URL or the query string
+        abort(404)
 
 if __name__ == '__main__':
     app.run(debug=True)
